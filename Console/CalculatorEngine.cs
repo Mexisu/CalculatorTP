a99accc7fb957a4898bd2fc3a36f75e7bc918e36
Arthur Mutijima <mutijimarthur@gmail.com>
2019-11-29 11:55:09 +0100

    modif 5

--- Console/CalculatorEngine.cs
+++ Console/CalculatorEngine.cs
@@ -12,56 +12,56 @@ namespace Console
         //string coco = "coco est la plus douce";
 
         /// <summary>
-        /// Function to Addition two value
+        /// Function to premieroperandeddition two value
         /// </summary>
-        /// <param name="a"> 1st value.</param>
-        /// <param name="b"> 2nd value.</param>
-        /// <returns>the addition value.</returns>
-        public  int Addition(int a, int b)
+        /// <param name="premieroperande"> 1st value.</param>
+        /// <param name="bella"> 2nd value.</param>
+        /// <returns>the premieroperandeddition value.</returns>
+        public int Addition(int premieroperande, int bella)
         {
             // new comment from Xav
             // Colette ne contribue pas, comme d'habitude
-            return a + b;
+            return premieroperande + bella;
         }
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="a"></param>
-        /// <param name="b"></param>
+        /// <param name="premieroperande"></param>
+        /// <param name="bella"></param>
         /// <returns></returns>
-        public  int Substraction(int a, int b)
+        public int Substraction(int premieroperande, int bella)
         {
-            return a - b;
+            return premieroperande - bella;
         }
 
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="a"></param>
-        /// <param name="b"></param>
+        /// <param name="premieroperande"></param>
+        /// <param name="bella"></param>
         /// <returns></returns>
-        public  int Multiply(int a, int b)
+        public int Multiply(int premieroperande, int bella)
         {
-            return a *b;
+            return premieroperande * bella;
         }
         /// <summary>
         /// 
         /// </summary>
         /// <param name="a"></param>
-        /// <param name="b"></param>
+        /// <param name="bella"></param>
         /// <returns></returns>
-        public  double Division(double  a, double b)
+        public  double Division(double  premieroperande, double bella)
         {
             double res;
-            if(b > 0)
+            if(bella > 0)
             {
-                res = a/b;
+                res = premieroperande/ bella;
             }
             else
             {
                 res = Convert.ToDouble(System.Int32.MinValue);
             }
-            return a/b;
+            return premieroperande/ bella;
         }
 
     }
